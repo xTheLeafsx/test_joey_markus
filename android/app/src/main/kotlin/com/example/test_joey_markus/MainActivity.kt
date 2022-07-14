3c7e60f73256df6eaa4f52eb91a9c926891f91c7
Markus <markusendres31@gmail.com>
2022-07-14 19:37:30 +0200

    Stand 1407 7pm

--- android/app/src/main/kotlin/com/example/test_joey_markus/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_joey_markus/MainActivity.kt
@@ -4,5 +4,3 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-class test6575: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,36 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'myCar - die Verwaltungsapp für Dein Auto!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0x69292929),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'myCar - die Verwaltungsapp für Dein Auto!'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  
   final String title;
 
   @override
@@ -48,68 +31,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final controller = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: PageView(
+        controller: controller,
+        children: const [
+        DetailPage(headline: 'Heute'),
+        DetailPage(headline: 'Gesterb'),
+      ],)
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key, required this.headline}) : super(key: key);
+
+  final String headline;
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(0.0, 48.0, 0.0, 32.0),
+      child: Column(children: [
+        Text(widget.headline, style: const TextStyle(fontSize: 48.0, color: Colors.white)),
+        const TrackingElement(color: Color(0x2496EA05), iconData: Icons.directions_run, unit: 'Steps', max: 5000),
+        const TrackingElement(color: Color(0xF913D7A3), iconData: Icons.local_drink, unit: 'l', max: 3),
+        const TrackingElement(color: Color(0xDD811285), iconData: Icons.fastfood, unit: 'pieces', max: 1800),
+      ],),
+    );
+  }
+}
+
+
+class TrackingElement extends StatefulWidget {
+  const TrackingElement({Key? key, required this.color, required this.iconData, 
+    required this.unit, required this.max }) : super(key: key);
+
+  final Color color;
+  final IconData iconData;
+  final String unit;
+  final double max;
+  
+  @override
+  State<TrackingElement> createState() => _TrackingElementState();
+}
+
+class _TrackingElementState extends State<TrackingElement> {
+
+  int _counter = 0;
+  double _progress = 0;
+   _incrementCounter() {
+  setState(() {
+      _counter += 200;
+      _progress = _counter / widget.max;  
+  });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: _incrementCounter(),
+        child: Column(
+      children:  <Widget>[
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(32.0, 64.0, 32.0, 24.0),
+          child: Row(children: <Widget>[
+            Icon(widget.iconData, color: Colors.white70, size: 50),
+            Text(
+                '$_counter.toString() / ${widget.max.toInt()} $widget.unit',
+                style: const TextStyle(color: Colors.white70, fontSize: 32)
+            ),
+          ],),
+        ),
+        LinearProgressIndicator(
+          value: _progress,
+          color: widget.color,
+          backgroundColor: const Color(0xC7EEF3EC),
+          minHeight: 12.0,
+        )
+      ],
+    ),);
+  }
+}
+
