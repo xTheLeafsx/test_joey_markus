333baf25b3d8aa1d2b069301c113ba4c880638ce
joeyhvh <96084957+joeyhvh@users.noreply.github.com>
2022-07-24 16:52:43 +0200

    master

--- test/Main_Menu.dart
+++ test/Main_Menu.dart
@@ -7,12 +7,16 @@ void main(){
 class Main_Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Main Menu',
-        home: Scaffold(
-            child: Ink.image(image: image),
+    return const Scaffold(
+      body: Center(
+              child: Image(
+                  image: AssetImage('Source/Logo.jpeg'),
+                height: 400,
+                width: 400,
+                alignment: Alignment(0.0, -0.5),
+              ),
           ),
-        ),
+      ),
     );
   }
 }
